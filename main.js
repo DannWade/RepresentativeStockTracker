12439c78f5cecd66b5bd7c564da84aa2bd8995e8
cheekygerold <spanishoceango@gmail.com>
2022-09-22 20:24:41 -0700

    add color,font style, drop down menu for districts

--- main.js
+++ main.js
@@ -29,27 +29,25 @@ fetch("https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/all_tran
 }) 
 
 
-// search by representative
+// search by district
 
-document.querySelector('button').addEventListener('click',search)
 
-function search(click){
-    // let searchName = document.querySelector('input').value
-    // alert(searchName) 
-    fetch("https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/all_transactions.json")
-        .then(res => res.json())
-        .then(data =>{  
-            // console.log(data)
-            let searchRep = data.filter((x,i) => x.representative === "Hon. Virginia Foxx")
-            console.log(searchRep)
-            // document.querySelector('.representative').innerText = searchRep[0].representative
-            // document.querySelector('.ticker').innerText = data[0].asset_description
-            // document.querySelector('.capitalGains').innerText = data[0].cap_gains_over_200_usd
-            // document.querySelector('.stockTransactions').innerText = data[0].amount   
-            // document.querySelector('.type').innerText = data[0].type  
-            // document.querySelector('.date').innerText = data[0].transaction_date   
-        })
-        .catch(err =>{
-            console.log(`error ${err}`)
-    }) 
-}
+
+fetch("https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/all_transactions.json")
+.then(res => res.json())
+.then(data =>{  
+    function removeDuplicates(arr) {
+        return arr.filter((item,
+            index) => arr.indexOf(item) === index);
+    }
+    let districts = []
+    data.forEach((x,i) => districts.push(x.district))
+    let newdistricts = removeDuplicates(districts)
+    let sortedDistricts = newdistricts.sort((a,b)=> a>b?1:-1)
+    console.log(sortedDistricts)
+    let dropdown = document.querySelector('.districtList')
+    sortedDistricts.forEach(element => dropdown.insertAdjacentHTML('beforeend',`<option value=${element}>${element}</option>`))
+})
+.catch(err =>{
+    console.log(`error ${err}`)
+}) 
